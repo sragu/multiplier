149b38a4111a718bb7b5e177901c383ca3f4de66
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2016-07-20 12:26:48 +1000

    iterator with seed

--- src/xyz/sragu/Main.java
+++ src/xyz/sragu/Main.java
@@ -1,6 +1,7 @@
 package xyz.sragu;
 
 import java.util.Iterator;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -8,10 +9,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final Stream<PeasantTable> table = StreamSupport.stream(new PeasantTable(20, 3).spliterator(), false);
-        final long result = table.filter(t -> !t.excluded).mapToLong(t -> t.first).sum();
+        final Stream<PeasantTable> table = StreamSupport.stream(new PeasantTable(20, 20).spliterator(), false);
+        final long result = table.peek(log()).filter(t -> !t.excluded).mapToLong(t -> t.first).sum();
 
-        System.out.println(result);
+        System.out.printf("result: %s \n", result);
+    }
+
+    private static Consumer<PeasantTable> log() {
+        return t -> System.out.printf("%s, %s \n", t.first, t.second);
     }
 }
 
@@ -28,16 +33,18 @@ class PeasantTable implements Iterable<PeasantTable> {
     @Override
     public Iterator<PeasantTable> iterator() {
         return new Iterator<PeasantTable>() {
-            private PeasantTable current = new PeasantTable(first / 2, second * 2);
+            private PeasantTable seed = new PeasantTable(first, second);
+            private PeasantTable next = seed;
 
             @Override
             public boolean hasNext() {
-                return current.second >= 1;
+                return next == seed ? true : next.second > 0;
             }
 
             @Override
             public PeasantTable next() {
-                current = new PeasantTable(current.first * 2, current.second / 2);
+                PeasantTable current = next;
+                next = new PeasantTable(next.first * 2, next.second / 2);
                 return current;
             }
         };
