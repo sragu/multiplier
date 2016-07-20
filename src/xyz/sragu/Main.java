7a47a150a41ae65235eaddaa6f50860b41eb41dd
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2016-07-20 18:04:47 +1000

    not implementing iterables

--- src/xyz/sragu/Main.java
+++ src/xyz/sragu/Main.java
@@ -7,65 +7,66 @@ import java.util.stream.StreamSupport;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("result: %s \n", Multiplier.multiply(20, 3));
+        System.out.printf("result: %s \n", Multiplier.multiply(20, 30));
     }
 }
 
 class Multiplier {
     public static long multiply(long one, long two) {
-        final PeasantTable peasantTable = new PeasantTable(one, two);
 
-        return createStreamFrom(peasantTable)
+        return PeasantTables.stream(PeasantTables.generate(one, two))
                 .peek(System.out::println)
-                .mapToLong(PeasantTable::resultValue)
+                .mapToLong(PeasantTables.PeasantTable::resultValue)
                 .sum();
-
-    }
-
-    private static Stream<PeasantTable> createStreamFrom(Iterable<PeasantTable> iterable) {
-        return StreamSupport.stream(iterable.spliterator(), false);
     }
 }
 
-/**
- * Using custom iterator (old school for generator) to generate peasant tables
- */
-class PeasantTable implements Iterable<PeasantTable> {
-    private final long first, second;
-    private final boolean excluded;
-
-    public PeasantTable(long first, long second) {
-        this.first = first;
-        this.second = second;
-        this.excluded = (second % 2 == 0);
+class PeasantTables {
+    static Iterable<PeasantTable> generate(final long first, final long second) {
+        return () -> new PeasantTableIterator(new PeasantTable(first, second));
     }
 
-    @Override
-    public Iterator<PeasantTable> iterator() {
-        return new Iterator<PeasantTable>() {
-            private PeasantTable seed = new PeasantTable(first, second);
-            private PeasantTable next = seed;
-
-            @Override
-            public boolean hasNext() {
-                return next == seed ? true : next.second > 0;
-            }
-
-            @Override
-            public PeasantTable next() {
-                PeasantTable current = next;
-                next = new PeasantTable(current.first * 2, current.second / 2);
-                return current;
-            }
-        };
+    static Stream<PeasantTable> stream(Iterable<PeasantTable> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
     }
 
-    @Override
-    public String toString() {
-        return String.format("%s, %s (excluded=%s)", first, second, excluded);
+    static class PeasantTableIterator implements Iterator<PeasantTable> {
+        private PeasantTable next;
+
+        PeasantTableIterator(PeasantTable seed) {
+            this.next = seed;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next.second > 0;
+        }
+
+        @Override
+        public PeasantTable next() {
+            PeasantTable current = next;
+            next = new PeasantTable(current.first * 2, current.second / 2);
+            return current;
+        }
     }
 
-    public long resultValue() {
-        return excluded ? 0 : first;
+    static class PeasantTable {
+        private final long first, second;
+        private final boolean excluded;
+
+        public PeasantTable(long first, long second) {
+            this.first = first;
+            this.second = second;
+            this.excluded = (second % 2 == 0);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s, %s (excluded=%s)", first, second, excluded);
+        }
+
+        public long resultValue() {
+            return excluded ? 0 : first;
+        }
     }
 }
