fee9606cd242e563151e1d9deefe656e78b436f7
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2016-07-27 22:15:35 +1000

    format the code

--- src/javascript/multiplier.js
+++ src/javascript/multiplier.js
@@ -1,9 +1,8 @@
 export function multiplier() {
 
-return {
-  multiply: function(a, b) {
-    return 12
+  return {
+    multiply: function(a, b) {
+      return 12
+    }
   }
-}
-
 }
--- src/javascript/multiplier_spec.js
+++ src/javascript/multiplier_spec.js
@@ -1,13 +1,12 @@
 import { multiplier } from './multiplier'
 import { expect } from 'chai'
 
-describe('multiplier', function(){
+describe('multiplier', () => {
 
-it('should multiply', function(){
-  var m = multiplier()
-  var result = m.multiply(3,4)
+  it('should multiply', () => {
+    var m = multiplier()
+    var result = m.multiply(3, 4)
 
-  expect(result).to.equal(12)
-})
-
+    expect(result).to.equal(12)
+  })
 })
