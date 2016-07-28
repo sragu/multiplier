fe6d89076a4e23a54597d1e9a1aa8ff7d2aabc05
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2016-07-28 12:10:12 +1000

    peasant table implementation with generators

--- src/javascript/multiplier.js
+++ src/javascript/multiplier.js
@@ -1,14 +1,26 @@
-function * peasantTables (a, b) {
-  if (b > 0) {
-    yield [a * 2, b / 2]
-  }
-}
-
-
 export function multiplier () {
+
+  /** Generator for peasantTables */
+  function * peasantTables (a, b) {
+    yield [a, b]
+
+    while (b > 0) {
+      yield [a = a * 2, b = Math.floor(b / 2)]
+    }
+  }
+
+  function valueOfEntry (first, second) {
+    return (second % 2 === 0) ? 0 : first
+  }
+
   return {
     multiply: function (a, b) {
-      return a * b
+      var result = 0
+      for (let [first, second] of peasantTables(a, b)) {
+        // console.log([first, second])
+        result += valueOfEntry(first, second)
+      }
+      return result
     }
   }
 }
