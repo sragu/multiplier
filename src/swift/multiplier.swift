36a75a62b827f58a72300d799517d527eeeb9732
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2016-08-01 00:51:32 +1000

    make struct readonly and cleanup

--- src/swift/multiplier.swift
+++ src/swift/multiplier.swift
@@ -1,15 +1,15 @@
 struct Entry {
-    var first: UInt64
-    var second: UInt64
+    let first: UInt64
+    let second: UInt64
 }
 
 class PeasantTables: Sequence, IteratorProtocol {
     var current:Entry
-    
+
     init(seed:Entry) {
         current = seed
     }
-    
+
     func next() -> Entry? {
         guard current.second > 0 else { return nil }
         defer { current = Entry(first: current.first * 2, second: current.second / 2) }
@@ -18,9 +18,9 @@ class PeasantTables: Sequence, IteratorProtocol {
 }
 
 func multiply(a: UInt64, b: UInt64) -> UInt64  {
-    let evenTableValues : (Entry) -> Bool =  { $0.second % 2 != 0 }
-    let firstValue : (Entry) -> UInt64 = { $0.first }
-    
+    let skipEvenTables : (Entry) -> Bool =  { $0.second % 2 != 0 }
+    let collectFirst : (Entry) -> UInt64 = { $0.first }
+
     let tables = PeasantTables(seed: Entry(first: a, second: b))
-    return tables.filter(evenTableValues).map(firstValue).reduce(0, +)
+    return tables.filter(skipEvenTables).map(collectFirst).reduce(0, +)
 }
