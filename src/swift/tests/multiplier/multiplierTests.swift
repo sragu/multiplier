f28f13255432d4b8324c5e8013b2cc9c39fd4d35
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2016-08-01 04:26:14 +1000

    change folder name to lower case

---
+++ src/swift/tests/multiplier/multiplierTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import multiplier
+
+class multiplierTests: XCTestCase {
+    func testMultiplication() {
+        XCTAssertEqual(multiplier.multiply(a:3, b:4), 12)
+        XCTAssertEqual(multiplier.multiply(a:30, b:4), 120)
+        XCTAssertEqual(multiplier.multiply(a:3, b:40), 120)
+    }
+    
+    func testMultiplicationEdgeCases() {
+        XCTAssertEqual(multiplier.multiply(a:0, b:0), 0)
+        XCTAssertEqual(multiplier.multiply(a:0, b:5), 0)
+        XCTAssertEqual(multiplier.multiply(a:43, b:0), 0)
+        XCTAssertEqual(multiplier.multiply(a:1, b:1), 1)
+    }
+    
+    static var allTests : [(String, (multiplierTests) -> () throws -> Void)] {
+        return [
+            ("testMultiplication", testMultiplication),
+            ("testMultiplicationEdgeCases", testMultiplicationEdgeCases)
+        ]
+    }
+}
