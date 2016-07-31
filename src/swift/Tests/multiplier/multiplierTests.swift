f88d41f6fae3b35582599e8701879133591ba566
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2016-08-01 04:14:53 +1000

    formatted swift code

--- src/swift/Tests/multiplier/multiplierTests.swift
+++ src/swift/Tests/multiplier/multiplierTests.swift
@@ -3,18 +3,18 @@ import XCTest
 
 class multiplierTests: XCTestCase {
     func testMultiplication() {
-          XCTAssertEqual(multiplier.multiply(a:3, b:4), 12)
-          XCTAssertEqual(multiplier.multiply(a:30, b:4), 120)
-          XCTAssertEqual(multiplier.multiply(a:3, b:40), 120)
+        XCTAssertEqual(multiplier.multiply(a:3, b:4), 12)
+        XCTAssertEqual(multiplier.multiply(a:30, b:4), 120)
+        XCTAssertEqual(multiplier.multiply(a:3, b:40), 120)
     }
-
+    
     func testMultiplicationEdgeCases() {
         XCTAssertEqual(multiplier.multiply(a:0, b:0), 0)
         XCTAssertEqual(multiplier.multiply(a:0, b:5), 0)
         XCTAssertEqual(multiplier.multiply(a:43, b:0), 0)
         XCTAssertEqual(multiplier.multiply(a:1, b:1), 1)
     }
-
+    
     static var allTests : [(String, (multiplierTests) -> () throws -> Void)] {
         return [
             ("testMultiplication", testMultiplication),
--- src/swift/multiplier.swift
+++ src/swift/multiplier.swift
@@ -5,11 +5,11 @@ struct Entry {
 
 class PeasantTables: Sequence, IteratorProtocol {
     var current:Entry
-
+    
     init(seed:Entry) {
         current = seed
     }
-
+    
     func next() -> Entry? {
         guard current.second > 0 else { return nil }
         defer { current = Entry(first: current.first * 2, second: current.second / 2) }
@@ -20,7 +20,7 @@ class PeasantTables: Sequence, IteratorProtocol {
 func multiply(a: UInt64, b: UInt64) -> UInt64  {
     let skipEvenTables : (Entry) -> Bool =  { $0.second % 2 != 0 }
     let collectFirst : (Entry) -> UInt64 = { $0.first }
-
+    
     let tables = PeasantTables(seed: Entry(first: a, second: b))
     return tables.filter(skipEvenTables).map(collectFirst).reduce(0, +)
 }
